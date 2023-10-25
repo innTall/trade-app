2bb822ebf320b5d339909e0f98a97bc5243df01c
innTall <avg999.trade@gmail.com>
2023-10-25 11:45:49 +0200

    head&foot menu punkt lighting

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,21 +1,29 @@
 <script setup>
 import FooterIcons from "./layouts/FooterIcons.vue";
+import { ref } from "vue";
 import {
-    QuestionMarkCircleIcon,
-    WrenchScrewdriverIcon,
-    CurrencyDollarIcon,
+  QuestionMarkCircleIcon,
+  WrenchScrewdriverIcon,
+  CurrencyDollarIcon,
 } from "@heroicons/vue/24/outline";
 const footerItems = {
-    about: QuestionMarkCircleIcon,
-    settings: WrenchScrewdriverIcon,
-    info: CurrencyDollarIcon,
+  about: QuestionMarkCircleIcon,
+  settings: WrenchScrewdriverIcon,
+  info: CurrencyDollarIcon,
 };
+const currentPage = ref("about");
 </script>
 
 <template>
   <footer class="sticky bottom-0 z-10">
     <ul class="flex justify-around p-2 border-t border-green-600">
-      <FooterIcons v-for="(icon, page) in footerItems" :key="page" :href="`#${page}`">
+      <FooterIcons
+        v-for="(icon, page) in footerItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-800 pointer-events-none': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </FooterIcons>
     </ul>
--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,31 +1,38 @@
 <script setup>
-import LogoItem from './layouts/LogoItem.vue';
-import HeaderIcons from './layouts/HeaderIcons.vue';
+import LogoItem from "./layouts/LogoItem.vue";
+import HeaderIcons from "./layouts/HeaderIcons.vue";
+import { ref } from 'vue';
 import {
-    QueueListIcon,
-    ArrowTrendingUpIcon,
-    TableCellsIcon,
-    ShoppingCartIcon
+  QueueListIcon,
+  ArrowTrendingUpIcon,
+  TableCellsIcon,
+  ShoppingCartIcon,
 } from "@heroicons/vue/24/outline";
 const headerItems = {
   watchlist: QueueListIcon,
   chart: ArrowTrendingUpIcon,
   options: TableCellsIcon,
-  trade: ShoppingCartIcon
+  trade: ShoppingCartIcon,
 };
+const currentPage = ref('watchlist');
 </script>
 
 <template>
   <header
-    class="sticky top-0 p-2 z-20 flex justify-between items-center border-b border-green-600"
+    class="sticky top-0 z-20 flex justify-between items-center border-b border-green-600"
   >
-    <LogoItem />
-    <ul class="flex items-center gap-5">
-      <HeaderIcons v-for="(icon, page) in headerItems" :key="page" :href="`#${page}`">
+    <LogoItem class="p-2"/>
+    <ul class="flex pr-2 items-center gap-5">
+      <HeaderIcons
+        v-for="(icon, page) in headerItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-900 p-1 border border-green-600 pointer-events-none': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </HeaderIcons>
     </ul>
   </header>
 </template>
-
 <style scoped></style>
