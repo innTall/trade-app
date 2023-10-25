1f4080026ca374d8b930c9a57ec15ba0547877a3
innTall <avg999.trade@gmail.com>
2023-10-25 09:09:41 +0200

    dynamic header & footer

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,17 @@ import {
     WrenchScrewdriverIcon,
     CurrencyDollarIcon,
 } from "@heroicons/vue/24/outline";
-const headerItems = ['watchlist', 'chart', 'options', 'trade'];
-const footerItems = ['about', 'settings', 'info'];
+const headerItems = {
+  watchlist: QueueListIcon,
+  chart: ArrowTrendingUpIcon,
+  options: TableCellsIcon,
+  trade: ShoppingCartIcon
+};
+const footerItems = {
+  about: QuestionMarkCircleIcon,
+  settings: WrenchScrewdriverIcon,
+  info: CurrencyDollarIcon
+};
 </script>
 
 <template>
@@ -30,12 +39,9 @@ const footerItems = ['about', 'settings', 'info'];
       </div>
     </div>
     <ul class="flex items-center gap-5">
-      <li v-for="page in headerItems" :key="page" class="flex-1">
+      <li v-for="icon, page in headerItems" :key="page" class="flex-1">
         <a :href="`#${page}`" class="flex flex-col text-xs capitalize items-center">
-          <QueueListIcon v-if="page === 'watchlist'" class="h-6 w-6" />
-          <ArrowTrendingUpIcon v-else-if="page === 'chart'" class="h-6 w-6" />
-          <TableCellsIcon v-else-if="page === 'options'" class="h-6 w-6" />
-          <ShoppingCartIcon v-else class="h-6 w-6" /> {{ page }}
+          <component :is="icon" class="h-6 w-6" /> {{ page }}
         </a>
       </li>
     </ul>
@@ -43,11 +49,9 @@ const footerItems = ['about', 'settings', 'info'];
   <main class="flex flex-grow flex-col"></main>
   <footer class="sticky bottom-0 z-10">
     <ul class="flex justify-around p-2 border-t border-green-600">
-      <li v-for="page in footerItems" :key="page" class="flex-1">
+      <li v-for="icon, page in footerItems" :key="page" class="flex-1">
         <a :href="`#${page}`" class="flex flex-col text-xs capitalize items-center">
-          <QuestionMarkCircleIcon v-if="page === 'about'" class="h-6 w-6" />
-          <WrenchScrewdriverIcon v-else-if="page === 'settings'" class="h-6 w-6" />
-          <CurrencyDollarIcon v-else class="h-6 w-6" /> {{ page }}
+          <component :is="icon" class="h-6 w-6" /> {{ page }}
         </a> 
       </li>
     </ul>
